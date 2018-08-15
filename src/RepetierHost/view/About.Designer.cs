77af5850d343e9815491686ce586e1573968b9de
Djeefther Souza Albuquerque <dsa2@cin.ufpe.br>
2018-08-14 23:33:27 -0300

    BIO-80 - change link to site with updates

--- src/RepetierHost/view/About.Designer.cs
+++ src/RepetierHost/view/About.Designer.cs
@@ -87,7 +87,7 @@
             this.linkLabel1.Size = new System.Drawing.Size(128, 13);
             this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://www.repetier.com";
+            this.linkLabel1.Text = "http://duelaser.com/";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // label2
