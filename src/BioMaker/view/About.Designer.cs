3a22605980d35267e06232e9cb27c7740203d260
Djeefther <djeefther@gmail.com>
2018-08-20 19:31:17 -0300

    BIO-27 - changing about date - partial

--- src/BioMaker/view/About.Designer.cs
+++ src/BioMaker/view/About.Designer.cs
@@ -43,28 +43,31 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(137, 126);
+            this.pictureBox1.Size = new System.Drawing.Size(171, 158);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // textLicence
             // 
-            this.textLicence.Location = new System.Drawing.Point(13, 165);
+            this.textLicence.Location = new System.Drawing.Point(16, 206);
+            this.textLicence.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textLicence.Multiline = true;
             this.textLicence.Name = "textLicence";
             this.textLicence.ReadOnly = true;
             this.textLicence.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textLicence.Size = new System.Drawing.Size(592, 235);
+            this.textLicence.Size = new System.Drawing.Size(739, 293);
             this.textLicence.TabIndex = 1;
             this.textLicence.Text = resources.GetString("textLicence.Text");
             // 
             // buttonOK
             // 
-            this.buttonOK.Location = new System.Drawing.Point(269, 414);
+            this.buttonOK.Location = new System.Drawing.Point(336, 518);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonOK.Name = "buttonOK";
-            this.buttonOK.Size = new System.Drawing.Size(80, 22);
+            this.buttonOK.Size = new System.Drawing.Size(100, 28);
             this.buttonOK.TabIndex = 2;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
@@ -73,18 +76,20 @@
             // labelLicenceAndLibraries
             // 
             this.labelLicenceAndLibraries.AutoSize = true;
-            this.labelLicenceAndLibraries.Location = new System.Drawing.Point(14, 145);
+            this.labelLicenceAndLibraries.Location = new System.Drawing.Point(18, 181);
+            this.labelLicenceAndLibraries.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelLicenceAndLibraries.Name = "labelLicenceAndLibraries";
-            this.labelLicenceAndLibraries.Size = new System.Drawing.Size(108, 13);
+            this.labelLicenceAndLibraries.Size = new System.Drawing.Size(144, 17);
             this.labelLicenceAndLibraries.TabIndex = 3;
             this.labelLicenceAndLibraries.Text = "Licence and Libraries";
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(157, 25);
+            this.linkLabel1.Location = new System.Drawing.Point(196, 31);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(106, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(133, 17);
             this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "http://duelaser.com/";
@@ -93,25 +98,28 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(157, 12);
+            this.label2.Location = new System.Drawing.Point(196, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(173, 13);
+            this.label2.Size = new System.Drawing.Size(231, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "DUE BioPrint (C) 2018 - DUE Laser";
             // 
             // labelRepetierInfo
             // 
-            this.labelRepetierInfo.Location = new System.Drawing.Point(160, 52);
+            this.labelRepetierInfo.Location = new System.Drawing.Point(200, 65);
+            this.labelRepetierInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelRepetierInfo.Name = "labelRepetierInfo";
-            this.labelRepetierInfo.Size = new System.Drawing.Size(445, 86);
+            this.labelRepetierInfo.Size = new System.Drawing.Size(556, 108);
             this.labelRepetierInfo.TabIndex = 6;
-            this.labelRepetierInfo.Text = resources.GetString("labelRepetierInfo.Text");
+            this.labelRepetierInfo.Text = "DUE BioPrint (C) 2018 - DUE Laser\r\nhttp://duelaser.com/\r\n\r\nThis Software were dev" +
+    "eloped to controll the 3D bioprint \"DUE BioPrint\"";
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(618, 448);
+            this.ClientSize = new System.Drawing.Size(772, 560);
             this.ControlBox = false;
             this.Controls.Add(this.labelRepetierInfo);
             this.Controls.Add(this.label2);
@@ -121,6 +129,7 @@
             this.Controls.Add(this.textLicence);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "About";
             this.ShowInTaskbar = false;
             this.Text = "About DUE BioPrint";
